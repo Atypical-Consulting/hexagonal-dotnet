f1aa03963a37d2dfd79f9be6b7241529b0d324d3
Philippe Matray <phmatray@gmail.com>
2026-02-28 15:35:04 +0100

    fix: replace Swashbuckle with built-in ASP.NET Core OpenAPI (#9)
    Swashbuckle.AspNetCore is incompatible with .NET 10. Replace it with the
    built-in Microsoft.AspNetCore.OpenApi package:
    
    - Remove Swashbuckle.AspNetCore package reference
    - Add Microsoft.AspNetCore.OpenApi 10.0.3
    - Replace AddSwaggerGen/UseSwagger/UseSwaggerUI with AddOpenApi/MapOpenApi
    - Remove unused Microsoft.OpenApi.Models using directive

--- src/Application.WebAPI/Startup.cs
+++ src/Application.WebAPI/Startup.cs
@@ -4,7 +4,6 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.OpenApi.Models;
 
 namespace Ideator.API
 {
@@ -22,10 +21,7 @@ namespace Ideator.API
         {
             services.ConfigureDomainServices();
             services.AddControllers();
-            services.AddSwaggerGen(c =>
-            {
-                c.SwaggerDoc("v1", new OpenApiInfo {Title = "Application.WebAPI", Version = "v1"});
-            });
+            services.AddOpenApi();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -34,8 +30,6 @@ namespace Ideator.API
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Application.WebAPI v1"));
             }
 
             app.UseHttpsRedirection();
@@ -44,7 +38,11 @@ namespace Ideator.API
 
             app.UseAuthorization();
 
-            app.UseEndpoints(endpoints => { endpoints.MapControllers(); });
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+                endpoints.MapOpenApi();
+            });
         }
     }
 }
