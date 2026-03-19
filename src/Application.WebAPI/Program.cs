62e912340dab4200c097bcb186675af0db0a57e4
Philippe Matray <phmatray@gmail.com>
2026-03-19 15:09:45 +0100

    feat: integrate TheAppManager for modular startup configuration
    Replace the traditional Program.cs + Startup.cs pattern with
    TheAppManager's composable module system. Domain services and
    WebAPI concerns are separated into dedicated IAppModule classes.

--- src/Application.WebAPI/Program.cs
+++ src/Application.WebAPI/Program.cs
@@ -1,19 +1,8 @@
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Hosting;
+using Ideator.API.Modules;
+using TheAppManager.Startup;
 
-namespace Ideator.API
+AppManager.Start(args, modules =>
 {
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            CreateHostBuilder(args).Build().Run();
-        }
-
-        public static IHostBuilder CreateHostBuilder(string[] args)
-        {
-            return Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder => { webBuilder.UseStartup<Startup>(); });
-        }
-    }
-}
+    modules.Add<DomainServicesModule>();
+    modules.Add<WebApiModule>();
+});
